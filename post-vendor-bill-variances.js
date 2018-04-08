8b16e472b62c80c15bfe45c2a91c7787f623f353
Jasper Chan <jasperchan@gmail.com>
2018-04-08 15:09:35 -0700

    initial commit

---
+++ post-vendor-bill-variances.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name         Post Vendor Bill Variances
+// @namespace    https://system.na2.netsuite.com/
+// @version      0.1
+// @description  Enhancements to Post Vendor Bill Variances page
+// @author       You
+// @match        https://system.na2.netsuite.com/app/accounting/transactions/vendorbillvariance/postvendorbillvariances.nl?whence=
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // lifted from the inspecting the JS on the Post Vendor Bill Variances page
+    function setMachineLineState(linenum, state) {
+        nlapiSelectLineItem('bulkmachine', linenum, false);
+        if (nlapiGetLineItemField('bulkmachine', 'selected', linenum) != null) {
+            if (!nlapiGetLineItemDisabled('bulkmachine','selected', linenum) && true ) nlapiSetCurrentLineItemValue('bulkmachine', 'selected', state ? 'T' : 'F',false);
+        }
+        else setEncodedValue('bulkmachine',linenum,'selected',state ? 'T' : 'F');
+        if (nlapiGetLineItemField('bulkmachine', 'selected', linenum) != null) bulkmachineSyncRow(linenum,true,true);
+    }
+
+    function isMatch(s, ids) {
+        for (let i=0;i<ids.length;i++) {
+            if(s === `Purchase Order #MDPO-${ids[i]}` || s === `Bill #MDPO-${ids[i]}`) {
+                return ids[i];
+            }
+        }
+        return false;
+    }
+
+    function selectIds(ids) {
+        let lines = nlapiGetLineItemCount('bulkmachine');
+        for(let i=1;i<=lines;i++){
+            setMachineLineState(i, isMatch(getEncodedValue('bulkmachine',i,'trannum'), ids));
+        }
+    }
+
+    function findIds(ids) {
+        let lines = nlapiGetLineItemCount('bulkmachine');
+        let results = {};
+        for(let i=1;i<=lines;i++){
+            let matchId = isMatch(getEncodedValue('bulkmachine',i,'trannum'), ids);
+            if(matchId) {
+                results[matchId] = Math.floor((i-1)/100)+1;
+            }
+        }
+        return {
+            found: results,
+            missing: jQuery(ids).not(Object.keys(results)).get()
+        };
+    }
+
+    // append the button
+    let $button, $bar;
+    // first button
+    $button = jQuery('<td><table cellpadding="0" cellspacing="0" border="0" class="uir-button" style="margin-right:6px;cursor:hand;" role="presentation"><tbody><tr class="tabBnt"><td class="bntBgB"><input class="rndbuttoninpt bntBgT" type="button" value="Select POs"/></td></tr></tbody></table></td>');
+    $bar = jQuery('#tdbody_customize').closest('table').closest('tr');
+    $button.click(() => {
+        let poIds = prompt('Comma-separated list of POs to select:');
+        selectIds(poIds.split(',').map((s) => s.trim()));
+    });
+    $bar.append($button);
+    // second button
+    $button = jQuery('<td><table cellpadding="0" cellspacing="0" border="0" class="uir-button" style="margin-right:6px;cursor:hand;" role="presentation"><tbody><tr class="tabBnt"><td class="bntBgB"><input class="rndbuttoninpt bntBgT" type="button" value="Find POs"/></td></tr></tbody></table></td>');
+    $bar = jQuery('#tdbody_customize').closest('table').closest('tr');
+    $button.click(() => {
+        let poIds = prompt('Comma-separated list of POs to find:');
+        console.log(findIds(poIds.split(',').map((s) => s.trim())));
+    });
+    $bar.append($button);
+})();
---
+++ reconcile-account-statement.js
@@ -0,0 +1,80 @@
+// ==UserScript==
+// @name         Reconcile Account Statement
+// @require      https://cdn.jsdelivr.net/npm/lodash@4.17.5/lodash.min.js
+// @namespace    https://system.na2.netsuite.com/
+// @version      0.1
+// @description  Enhancements to Reconcile Account Statement page
+// @author       You
+// @match        https://system.na2.netsuite.com/erp/bankrec/edit/bankrec.nl?whence=
+// @grant        GM_setClipboard
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function qs(data) {
+        let ret = [];
+        for (let d in data)
+            ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+        return ret.join('&');
+    }
+
+    // action
+    let $a1 = jQuery('<a href="#">Copy Current Unmatched Page</a>').click(function(e) {
+        e.preventDefault();
+        let $grid = jQuery('.n-bankrec-imported-transactions-grid');
+        let clip = [];
+
+        let header = $grid.find('.n-w-datagrid__header-row').children().map(function() {
+            return jQuery(this).text();
+        }).toArray().slice(1).join('\t');
+        clip.push(header);
+        $grid.find('.n-w-datagrid__body-row').each(function() {
+            let row = jQuery(this).children().map(function() {
+                return jQuery(this).text();
+            }).toArray().slice(1).join('\t');
+            clip.push(row);
+        });
+        GM_setClipboard(clip.join('\n'), 'text');
+    });
+
+    // action
+    let $a2 = jQuery('<a href="#">Copy Unmatched PayPal Statement Transactions</a>').click(function(e) {
+        e.preventDefault();
+
+        // get date bounds
+        let start = jQuery('div[data-view-id=start-date]').data('date');
+        let finish = jQuery('div[data-view-id=end-date]').data('date');
+
+        // retrieve data and copy to clipboard
+        if(start && finish) {
+            start = start.substr(0,10);
+            finish = finish.substr(0,10);
+            jQuery.get('/erp/resources/importedtransactions.nl?' + qs({
+                accountId: 434,
+                isMatched: 'F',
+                start: start,
+                finish: finish,
+                sortBy: 'dDate',
+                sortDirection: 'ASC',
+                skip: 0,
+                pageSize: 10000
+            }), function(data) {
+                const clip = [];
+                const cols = ['date', 'transactionType', 'transactionNumber', 'payee', 'memo', 'amount'];
+                clip.push(cols.join('\t'));
+                _.forEach(data.resultSubset, (r) => {
+                    clip.push(_.at(r, cols).join('\t'));
+                });
+                GM_setClipboard(clip.join('\n'), 'text');
+            });
+        }
+    });
+
+    let $actions = jQuery('<div class="n-widget" style="font-size:10px;"><span>Actions: </span></div>');
+    $actions.append($a1);
+    $actions.append('&nbsp;');
+    $actions.append($a2);
+    jQuery('#application').before($actions);
+
+})();
